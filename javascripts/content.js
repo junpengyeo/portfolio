1767a8b5a1ef7501dcd22967e50062f135cba323
junpengyeo <junpengyeo@gmail.com>
2020-11-03 05:52:29 +0800

    aesthetic changes

--- javascripts/content.js
+++ javascripts/content.js
@@ -34,37 +34,54 @@ header.innerHTML = `<section>
 </ul>
 </section>`;
 
-contact.innerHTML=`<div class='block'>
-<div class='row'>
-  <div class='col-md-4'>
-    <div class='row'>
-      <div class='col-md-2'>
-        <div class='fa fa-building-o'></div>
+contact.innerHTML=`<section class='contact' id='contact'>
+<div class='row content'>
+  <div class='title-block'>
+    <div class='col-md-3 col-md-offset-1'>
+      <div class='row'>
+        <div class='col-md-2'>
+          <div class='fa fa-building-o'></div>
+        </div>
+        <div class='col-md-10'>
+          <p>Singapore</p>
+        </div>
       </div>
-      <div class='col-md-10'>
-        <p>Singapore</p>
+    </div>
+    <div class='col-md-3 col-md-offset-1'>
+      <div class='row'>
+        <div class='col-md-2'>
+          <div class='fa fa-phone'></div>
+        </div>
+        <div class='col-md-10'>
+          <p>(65) 8298 7130</p>
+        </div>
+      </div>
+    </div>
+    <div class='col-md-3 col-md-offset-1'>
+      <div class='row'>
+        <div class='col-md-2'>
+          <div class='fa fa-envelope-o'></div>
+        </div>
+        <div class='col-md-10'>
+          <p><a href='mailto:junpengyeo@gmail.com'>junpengyeo@gmail.com</a></p>
+        </div>
       </div>
     </div>
   </div>
-  <div class='col-md-4'>
-    <div class='row'>
-      <div class='col-md-2'>
-        <div class='fa fa-phone'></div>
-      </div>
-      <div class='col-md-10'>
-        <p>(65) 8298 7130</p>
-      </div>
-    </div>
-  </div>
-  <div class='col-md-4'>
-    <div class='row'>
-      <div class='col-md-2'>
-        <div class='fa fa-envelope-o'></div>
-      </div>
-      <div class='col-md-10'>
-        <p><a href='mailto:junpengyeo@gmail.com' style="color: #888888;">junpengyeo@gmail.com</a></p>
-      </div>
-    </div>
+  <div>
+    <div class='text-center' style="padding: 2rem;">
+      <a class="button button2 anchor" href="mailto:junpengyeo@gmail.com">Contact Now</a>
   </div>
 </div>
-</div>`
+</div>
+<div class='text-center title-block'>
+  <div class='social'>
+  <a class='icon fa fa-github' href='https://github.com/junpengyeo/'></a>
+  <a class='icon fa fa-facebook' href='https://www.facebook.com/yjpeng/'></a>
+  <a class='icon fa fa-linkedin' href='https://www.linkedin.com/in/junpengyeo/'></a>
+  </div>
+  <div class='copy'>
+  &copy; 2020 Jun Peng. All Rights Reserved.
+  </div>
+</div>
+</section>`
